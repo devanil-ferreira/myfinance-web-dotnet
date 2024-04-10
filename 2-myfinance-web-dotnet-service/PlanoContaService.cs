4445d709534c6bd26a0fa5020d5f006c5e4a25c2
devanil-ferreira <devanil.assis.ferreira@gmail.com>
2024-04-10 12:27:00 -0300

    add services

---
+++ 2-myfinance-web-dotnet-service/PlanoContaService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using myfinance_web_dotnet_service.Interfaces;
+
+namespace myfinance_web_dotnet_service
+{
+    public class PlanoContaService : IPlanoContaService
+    {
+        teste.
+    }
+}
